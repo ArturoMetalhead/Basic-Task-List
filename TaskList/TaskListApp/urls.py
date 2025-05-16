fb186bb0357f63fe773c6649f04df3467b35abaf
ArturoMetalhead <105013797+ArturoMetalhead@users.noreply.github.com>
2025-05-16 17:17:07 -0400

    feat: add and delete tasks functionality

--- TaskList/TaskListApp/urls.py
+++ TaskList/TaskListApp/urls.py
@@ -3,4 +3,6 @@ from TaskListApp import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('add-task/', views.add_task, name='add_task'),
+    path('delete-task/<int:task_id>/', views.delete_task, name='delete_task'),
 ]
--- TaskList/TaskListApp/views.py
+++ TaskList/TaskListApp/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render,HttpResponse
-
-# Create your views here.
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Task
 
 def home(request):
-    return render(request,'TaskListApp/home.html')
+
+    tasks = Task.objects.all().order_by('-created_at')
+    return render(request, 'TaskListApp/home.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title', '').strip()
+        if title:
+            Task.objects.create(title=title)
+    return redirect('home')
+
+
+def delete_task(request, task_id):
+    if request.method == 'POST':
+        task = get_object_or_404(Task, id=task_id)
+        task.delete()
+    return redirect('home')
