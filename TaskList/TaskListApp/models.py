b6db4fba1e2e68bc4f44498d43cbf5ad69216971
ArturoMetalhead <105013797+ArturoMetalhead@users.noreply.github.com>
2025-05-16 17:59:04 -0400

    refactor: Task model

--- TaskList/TaskListApp/models.py
+++ TaskList/TaskListApp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
